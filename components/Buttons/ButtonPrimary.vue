83052fae0cf5b25a3b8e23f2bf3722964805fadc
LuizAuGFer <luiz.augusto9922@outlook.com>
2023-11-30 07:21:42 -0300

    feat: primary button updated

--- components/Buttons/ButtonPrimary.vue
+++ components/Buttons/ButtonPrimary.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
-        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            {{ text }}
+        </button>
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        text: 'String'
+    }
+}
+</script>
